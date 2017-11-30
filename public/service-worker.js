53cb8adf57829ee464b001ee53e7f6025bc0f9f4
fegan104 <fegan104@gmail.com>
2017-11-30 17:52:09 -0500

    added back ground notification handler

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,11 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/3.9.0/firebase-app.js');
-importScripts('/__/firebase/3.9.0/firebase-messaging.js');
-importScripts('/__/firebase/init.js');
+const firebase = require('/__/firebase/3.9.0/firebase-app.js');
+require('/__/firebase/3.9.0/firebase-messaging.js');
+require('/__/firebase/init.js');
 
 const messaging = firebase.messaging();
-
 
 // If you would like to customize notifications that are received in the
 // background (Web app is closed or not in browser focus) then you should
@@ -19,6 +18,7 @@ messaging.setBackgroundMessageHandler(payload => {
     body: 'Background Message body.'
   };
 
+  // eslint-disable-line no-restricted-globals
   return self.registration.showNotification(notificationTitle,
       notificationOptions);
 });
